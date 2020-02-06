a26e05d91a65e646372497cb76f41f30e5d866d0
gosntvrdi <39313008+gosntvrdi@users.noreply.github.com>
2020-02-06 10:33:04 +0100

    Update artwork.py

--- artwork.py
+++ artwork.py
@@ -1,12 +1,16 @@
 import requests
 import dicttoxml
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def artworkXML():
+    url = ("https://public.radio.co/stations/s5ad4b474a/status")
+    response = requests.get(url).json()
+    xml = dicttoxml.dicttoxml(response)
+    xmlString = xml.decode(encoding='UTF-8')
+    with open("artwork.xml", "w") as f:
+        f.write(xmlString)
 
 
-url = ("https://public.radio.co/stations/s5ad4b474a/status")
-response = requests.get(url).json()
-xml = dicttoxml.dicttoxml(response)
-xmlString = xml.decode(encoding='UTF-8')
-with open("artwork.xml", "w") as f:
-    f.write(xmlString)
-
-
+scheduler = BlockingScheduler()
+scheduler.add_job(artworkXML, 'interval', seconds=6, max_instances=1)
+scheduler.start()
