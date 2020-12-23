084cc7dcc7e947f02fa1ca17f08e473a59590da7
gosntvrdi <39313008+gosntvrdi@users.noreply.github.com>
2020-12-23 12:26:22 +0100

    Update artwork.py

--- artwork.py
+++ artwork.py
@@ -1,12 +1,18 @@
 import requests
 import dicttoxml
+import json
+from urllib.request import urlopen
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def artworkXML():
     url = ("https://public.radio.co/stations/s5ad4b474a/status")
     response = requests.get(url).json()
-    xml = dicttoxml.dicttoxml(response)
+    urllink = urlopen(url).read()
+    result = json.loads(urllink)
+    xml = dicttoxml.dicttoxml(result)
+
     xmlString = xml.decode(encoding='UTF-8')
+    print(xmlString)
     with open("artwork.xml", "w") as f:
         f.write(xmlString)
 
